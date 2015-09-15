f7303f9a9c4f3d0b251fd3c4fccc49e2f4427e86
tiena2cva <tiena2cva@gmail.com>
2015-09-15 10:31:12 +0700

    Support negative

--- Calculator/Expression.cs
+++ Calculator/Expression.cs
@@ -8,8 +8,8 @@ namespace Calculator
 {
 	#region Base class
 	public abstract class Element
-    {
-        public virtual double Value { get; set; }
+	{
+		public virtual double Value { get; set; }
 
 		public static ListSupport listOp = new ListSupport();
 
@@ -26,53 +26,53 @@ namespace Calculator
 		public abstract Element Clone();
 	}
 
-    public abstract class Operator : Element
-    {
+	public abstract class Operator : Element
+	{
 		public List<Element> Inputs { get; set; }
 
-        public override double Value
-        {
-            get
-            {
-                return Calculate();
-            }
-        }
+		public override double Value
+		{
+			get
+			{
+				return Calculate();
+			}
+		}
 
-        public abstract double Calculate();
-        public int Level { get; set; }
+		public abstract double Calculate();
+		public int Level { get; set; }
 
-        public Operator()
-        {
-            Inputs = new List<Element>();
-        }
+		public Operator()
+		{
+			Inputs = new List<Element>();
+		}
 
-        public override Element Clone()
-        {
-            Operator other = (Operator)this.MemberwiseClone();
-            other.Inputs = new List<Element>();
-            return other;
-        }
+		public override Element Clone()
+		{
+			Operator other = (Operator)this.MemberwiseClone();
+			other.Inputs = new List<Element>();
+			return other;
+		}
 
-        public virtual void MakeInput(Stack<Element> stack)
-        {
-            Inputs.Clear();
-            Inputs.Add(stack.Pop());
-            Inputs.Add(stack.Pop());
-        }
+		public virtual void MakeInput(Stack<Element> stack)
+		{
+			Inputs.Clear();
+			Inputs.Add(stack.Pop());
+			Inputs.Add(stack.Pop());
+		}
 	}
 
-    public class Operand : Element
-    {
+	public class Operand : Element
+	{
 		public Operand()
 		{
 			Value = 0;
 		}
 
-        public Operand(double v)
-        {
-            Value = v;
-        }
-        public override double Value { get; set; }
+		public Operand(double v)
+		{
+			Value = v;
+		}
+		public override double Value { get; set; }
 
 		public override Element Clone()
 		{
@@ -86,93 +86,110 @@ namespace Calculator
 	#region  Normal operator
 
 	public class Add : Operator
-    {
-        public Add()
-            : base()
-        {
-            Level = 0;
-        }
-        public override double Calculate()
-        {
-            return Inputs[0].Value + Inputs[1].Value;
-        }
-    }
+	{
+		public Add()
+			: base()
+		{
+			Level = (int)OperatorLevel.Add;
+		}
+		public override double Calculate()
+		{
+			return Inputs[0].Value + Inputs[1].Value;
+		}
+	}
 
-    public class Minus : Operator
-    {
-        public Minus()
-            : base()
-        {
-            Level = 1;
-        }
-        public override double Calculate()
-        {
-            return Inputs[1].Value - Inputs[0].Value;
-        }
-    }
+	public class Minus : Operator
+	{
+		public Minus()
+			: base()
+		{
+			Level = (int)OperatorLevel.Minus;
+		}
+		public override double Calculate()
+		{
+			return Inputs[1].Value - Inputs[0].Value;
+		}
+	}
 
-    public class Mul : Operator
-    {
-        public Mul()
-            : base()
-        {
-            Level = 2;
-        }
-        public override double Calculate()
-        {
-            return Inputs[0].Value * Inputs[1].Value;
-        }
-    }
+	public class Mul : Operator
+	{
+		public Mul()
+			: base()
+		{
+			Level = (int)OperatorLevel.Mul;
+		}
+		public override double Calculate()
+		{
+			return Inputs[0].Value * Inputs[1].Value;
+		}
+	}
 
-    public class Div : Operator
-    {
-        public Div()
-            : base()
-        {
-            Level = 2;
-        }
-        public override double Calculate()
-        {
-            return Inputs[1].Value / Inputs[0].Value;
-        }
-    }
+	public class Div : Operator
+	{
+		public Div()
+			: base()
+		{
+			Level = (int)OperatorLevel.Div;
+		}
+		public override double Calculate()
+		{
+			return Inputs[1].Value / Inputs[0].Value;
+		}
+	}
 
-    public class CloseBracket : Operator
-    {
-        public CloseBracket()
-        {
-            Level = -1;
-        }
-        public override double Calculate()
-        {
-            return 0;
-        }
-    }
+	public class CloseBracket : Operator
+	{
+		public CloseBracket()
+		{
+			Level = (int)OperatorLevel.CloseBracket;
+		}
+		public override double Calculate()
+		{
+			return 0;
+		}
+	}
 
-    public class OpenBracket : Operator
-    {
-        public OpenBracket()
-        {
-            Level = -1;
-        }
-        public override double Calculate()
-        {
-            return 0;
-        }
-    }
+	public class OpenBracket : Operator
+	{
+		public OpenBracket()
+		{
+			Level = (int)OperatorLevel.OpenBracket;
+		}
+		public override double Calculate()
+		{
+			return 0;
+		}
+	}
 
-    public class Pow : Operator
-    {
-        public Pow() : base()
-        {
-            Level = 3;
-        }
+	public class Pow : Operator
+	{
+		public Pow() : base()
+		{
+			Level = (int)OperatorLevel.Pow;
+		}
 
-        public override double Calculate()
-        {
-            return Math.Pow(Inputs[1].Value, Inputs[0].Value);
-        }
-    }
+		public override double Calculate()
+		{
+			return Math.Pow(Inputs[1].Value, Inputs[0].Value);
+		}
+	}
+
+	public class Negative : Operator
+	{
+		public Negative() : base()
+		{
+			Level = (int)OperatorLevel.Negative;
+		}
+		public override double Calculate()
+		{
+			return -Inputs[0].Value;
+		}
+
+		public override void MakeInput(Stack<Element> stack)
+		{
+
+		}
+	}
 	#endregion
 
 	#region Function support
@@ -184,15 +201,15 @@ namespace Calculator
 	}
 
 	public class Sin : Function
-    {
-        public Sin() : base()
-        {
+	{
+		public Sin() : base()
+		{
 			NumArg = 1;
-        }
-        public override double Calculate()
-        {
-            return Math.Sin(Inputs[0].Value);
-        }
+		}
+		public override double Calculate()
+		{
+			return Math.Sin(Inputs[0].Value);
+		}
 	}
 
 	public class Logarithm : Function
@@ -268,20 +285,32 @@ namespace Calculator
 	}
 	#endregion
 
+	enum OperatorLevel
+	{
+		Add = 0,
+		OpenBracket = -1,
+		CloseBracket = -1,
+		Minus = 1,
+		Mul = 2,
+		Div = 2,
+		Pow = 3,
+		Negative = 0
+	}
 	public class ListSupport : Dictionary<string, Element>
-    {
-        public ListSupport()
-        {
+	{
+		public ListSupport()
+		{
+			//operator
 			Add("+", new Add());
 			Add("-", new Minus());
 			Add("*", new Mul());
-            Add("/", new Div());
-            Add("(", new OpenBracket());
-            Add(")", new CloseBracket());
-            Add("^", new Pow());
+			Add("/", new Div());
+			Add("(", new OpenBracket());
+			Add(")", new CloseBracket());
+			Add("^", new Pow());
 
 			//function
-            Add("sin", new Sin());
+			Add("sin", new Sin());
 			Add("cos", new Cos());
 			Add("tan", new Tan());
 			Add("logarithm", new Logarithm());
@@ -291,5 +320,5 @@ namespace Calculator
 			//const
 			Add("pi", new Pi());
 		}
-    }
+	}
 }
--- Calculator/Parse.cs
+++ Calculator/Parse.cs
@@ -6,42 +6,42 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Parse
-    {
+	class Parse
+	{
 		static string StrNumber = "0123456789.e";
 		static string StrOperator = "+-*/^";
 		static string StrEnd = "),";
 
-        static Operand getDouble(string s, ref int index)
-        {
-            string num = "";
-            char c = s[index];
-            while (StrNumber.IndexOf(c) != -1)
-            {
-                num += c;
-                if (++index == s.Length) break;
-                c = s[index];
-            }
+		static Operand getDouble(string s, ref int index)
+		{
+			string num = "";
+			char c = s[index];
+			while (StrNumber.IndexOf(c) != -1)
+			{
+				num += c;
+				if (++index == s.Length) break;
+				c = s[index];
+			}
 
-            return new Operand(double.Parse(num));
-        }
+			return new Operand(double.Parse(num));
+		}
 
-        static Operator getOperator(string s, ref int index)
-        {
-            return (Operator) Element.CreateElement(s[index++].ToString());
-        }
+		static Operator getOperator(string s, ref int index)
+		{
+			return (Operator)Element.CreateElement(s[index++].ToString());
+		}
 
 		static Function getFunction(string s, ref int index, string func)
 		{
-			Function f = (Function) Element.CreateElement(func);
-			for(int i = 0; i < f.NumArg; i++)
+			Function f = (Function)Element.CreateElement(func);
+			for (int i = 0; i < f.NumArg; i++)
 			{
-				if(s[index - 1] != '(' && s[index - 1] != ',') throw new Exception();
+				if (s[index - 1] != '(' && s[index - 1] != ',') throw new Exception();
 
 				Element e = Evaluate(s, ref index);
 				index++;
 				f.Inputs.Add(e);
-            }
+			}
 
 			if (s[index - 1] != ')') throw new Exception();
 
@@ -65,8 +65,8 @@ namespace Calculator
 					index++;
 					return getFunction(s, ref index, name);
 				}
-				
-				if((StrOperator + StrEnd).IndexOf(s[index]) != -1)
+
+				if ((StrOperator + StrEnd).IndexOf(s[index]) != -1)
 				{
 					return getConst(s, ref index, name);
 				}
@@ -76,64 +76,74 @@ namespace Calculator
 			return null;
 		}
 		public static Element Evaluate(string s, ref int i)
-        {
-            Stack<Element> element = new Stack<Element>();
-            Stack<Operator> op = new Stack<Operator>();
-            op.Push(new OpenBracket());
+		{
+			Stack<Element> element = new Stack<Element>();
+			Stack<Operator> op = new Stack<Operator>();
+			op.Push(new OpenBracket());
 
-            for (; i < s.Length;)
-            {
-                char c = s[i];
+			for (; i < s.Length;)
+			{
+				char c = s[i];
 				if (c == ' ')
 				{
 					i++;
 					continue;
 				}
-                if (StrNumber.IndexOf(c) != -1)
-                {
-                    element.Push(getDouble(s, ref i));
-                    continue;
-                }
+				if (StrNumber.IndexOf(c) != -1)
+				{
+					element.Push(getDouble(s, ref i));
+					continue;
+				}
 
 				if (StrOperator.IndexOf(c) != -1)
 				{
-					Operator temp = getOperator(s, ref i);
-					if (temp != null)
+					if(c == '-' && (i == 0 || (StrOperator + '(').IndexOf(s[i-1]) != -1))
 					{
-						while (temp.Level <= op.Peek().Level && !(temp is OpenBracket))
-						{
-							Operator o = op.Pop();
-							o.MakeInput(element);
-							element.Push(o);
-						}
-						op.Push(temp);
+						i++;
+						Negative n = new Negative();
+						n.Inputs.Add(Evaluate(s, ref i));
+						element.Push(n);
+						continue;
 					}
+					if (c == '+' && (i == 0 || (StrOperator + '(').IndexOf(s[i - 1]) != -1))
+					{
+						i++;
+						continue;
+					}
+					Operator temp = getOperator(s, ref i);
+					while (temp.Level <= op.Peek().Level && !(temp is OpenBracket))
+					{
+						Operator o = op.Pop();
+						o.MakeInput(element);
+						element.Push(o);
+					}
+					op.Push(temp);
 					continue;
 				}
-                if (c == '(')
-                {
-                    i++;
-                    element.Push(Evaluate(s, ref i));
+				if (c == '(')
+				{
+					i++;
+					element.Push(Evaluate(s, ref i));
 					continue;
-                }
-                if (StrEnd.IndexOf(c) != -1)
-                {
-                    while (!(op.Peek() is OpenBracket))
-                    {
-                        Operator o = op.Pop();
-                        o.MakeInput(element);
+				}
+				if (StrEnd.IndexOf(c) != -1)
+				{
+					while (!(op.Peek() is OpenBracket))
+					{
+						Operator o = op.Pop();
+						o.MakeInput(element);
 						element.Push(o);
-                    }
-                    op.Pop();
-                    break;
-                }
+					}
+					op.Pop();
+					break;
+				}
 
 				element.Push(getSpec(s, ref i));
-            }
+			}
 
 			if (element.Count != 1) throw new Exception();
 
 			return element.Pop();
-        }
-    }
+		}
+	}
 }
